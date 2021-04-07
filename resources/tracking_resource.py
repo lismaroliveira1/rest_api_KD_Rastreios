8e8d3fe400b1abef2149d9a766d54cdba306f43d
lismaroliveira1 <englismaroliveira@gmail.com>
2021-04-06 21:49:13 -0300

    add the get method on Tracking resource

--- resources/tracking_resource.py
+++ resources/tracking_resource.py
@@ -5,3 +5,19 @@ from models.tracking_model import TrackingModel
 class Trackings(Resource):
     def get(self):
         return {"trackings": [tracking.toJson() for tracking in TrackingModel.query.all()]}
+
+
+class Tracking(Resource):
+
+    arguments = reqparse.RequestParser()
+    arguments.add_argument('date', type=str, required=True)
+    arguments.add_argument('destiny', type=str)
+    arguments.add_argument('hour', type=str)
+    arguments.add_argument('origin', type=str)
+    arguments.add_argument('status', type=str)
+
+    def get(self, tracking_id):
+        tracking = TrackingModel.findTracking(tracking_id)
+        if tracking:
+            return tracking.toJson(), 200
+        return {"message": "tracking not found"}, 400
